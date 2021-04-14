4600912880c03ca1b02105a468d45feced23de05
Myron Wells <myronjwells@gmail.com>
2021-04-14 19:02:15 -0400

    Initial Commit

---
+++ Shared/TrickleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrickleApp.swift
+//  Shared
+//
+//  Created by Myron Wells on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct TrickleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
